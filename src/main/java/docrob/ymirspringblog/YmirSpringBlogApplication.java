b8e8ed9b907a64c6cec58554c5b35f5091a2a92b
Docrob <mark.robinson@codeup.com>
2023-03-23 09:20:52 -0500

    first commit

---
+++ src/main/java/docrob/ymirspringblog/YmirSpringBlogApplication.java
@@ -0,0 +1,13 @@
+package docrob.ymirspringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YmirSpringBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YmirSpringBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/docrob/ymirspringblog/YmirSpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package docrob.ymirspringblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YmirSpringBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
